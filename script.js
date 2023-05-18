008162f248fa263fb164ad1095538d8663861884
Ram Narayan <shukla.narayanram@gmail.com>
2023-05-18 11:44:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+'use strict';
+
+
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+addEventOnElem(navLinks, "click", closeNavbar);
+
+
+
+/**
+ * header active when scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const activeElem = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+
+
+addEventOnElem(window, "scroll", activeElem);
+var images = ["image1.jpg", "image2.jpg", "image3.jpg"];
+    
+var currentImageIndex = 0;
+var imageElement = document.getElementById("image");
+
+// Function to change the image
+function changeImage() {
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  imageElement.src = images[currentImageIndex];
+}
+
+// Time interval in milliseconds (e.g., 5 seconds)
+var interval = 5000;
+
+// Start the image rotation
+setInterval(changeImage, interval);
