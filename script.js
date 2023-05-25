d8252c30aa18826cb620008e9550ac69a8011379
Ram Narayan <shukla.narayanram@gmail.com>
2023-05-25 14:00:11 +0530

    slider

--- script.js
+++ script.js
@@ -1,6 +1,51 @@
 'use strict';
+var slider = document.getElementById("slider");
+var sliderWidth = slider.offsetWidth;
+var slideList = document.getElementById("slideWrap");
+var count = 1;
+var items = slideList.querySelectorAll("li").length;
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
 
+window.addEventListener('resize', function() {
+    sliderWidth = slider.offsetWidth;
+});
 
+function nextSlide(){
+    if(count < items) {
+        slideList.style.left = "-" + count * sliderWidth + "px";
+        count++;
+      }
+      else if(count = items) {
+        slideList.style.left = "0px";
+        count = 1;
+      }
+}
+
+function prevSlide(){
+    if(count > 1) {
+        count = count - 2;
+        slideList.style.left = "-" + count * sliderWidth + "px";
+        count++;
+      }
+      else if(count = 1) {
+        count = items - 1;
+        slideList.style.left = "-" + count * sliderWidth + "px";
+        count++;
+      }
+}
+
+next.addEventListener("click", function() {
+    nextSlide();
+});
+  
+prev.addEventListener("click", function() {
+    prevSlide();
+});
+
+setInterval(function() {
+    nextSlide()
+}, 5000);
 
 /**
  * add event on element
@@ -61,3 +106,5 @@ const activeElem = function () {
 }
 
 addEventOnElem(window, "scroll", activeElem);
+
+
